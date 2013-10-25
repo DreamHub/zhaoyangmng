0b3d786e71afcba05575d220187a93378824f23a
DreamHub <1169286799@qq.com>
2013-10-25 09:19:04 -0700

    师生

---
+++ src/com/zhaoyang/util/UtilForGeneratePeople.java
@@ -0,0 +1,16 @@
+package com.zhaoyang.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.zhaoyang.dao.NewsDao;
+import com.zhaoyang.dao.NoticeDao;
+import com.zhaoyang.dao.RuleDao;
+import com.zhaoyang.orm.News;
+import com.zhaoyang.orm.Notice;
+import com.zhaoyang.orm.Rule;
+
+public class UtilForGeneratePeople {
+	
+	
+}
