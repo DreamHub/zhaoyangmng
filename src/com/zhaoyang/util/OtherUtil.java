e217d94d4fb671441941f0dfa5ca4e23f727a458
DreamHub <1169286799@qq.com>
2013-10-25 09:06:37 -0700

    工具类-储院诚

--- src/com/zhaoyang/util/OtherUtil.java
+++ src/com/zhaoyang/util/OtherUtil.java
@@ -1,32 +1,49 @@
 package com.zhaoyang.util;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 
-import org.json.JSONObject;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.apache.struts2.ServletActionContext;
 
 public class OtherUtil {
 
 	/**
-		 * 读取输入流数据
-		 * @param inStream
-		 * @return
-		 */
-		public static byte[] read(InputStream inStream) throws Exception{
-			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-			byte[] buffer = new byte[1024];
-			int len = 0;
-			while( (len = inStream.read(buffer)) != -1 ){
-				outStream.write(buffer, 0, len);
-			}
-			inStream.close();
-			return outStream.toByteArray();
+	 * 读取输入流数据
+	 * 
+	 * @param inStream
+	 * @return
+	 */
+	public static byte[] read(InputStream inStream) throws Exception {
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+		int len = 0;
+		while ((len = inStream.read(buffer)) != -1) {
+			outStream.write(buffer, 0, len);
 		}
-		public static String getLocalityName(String googleJsonStr) throws Exception{
-			String str=new JSONObject(googleJsonStr).getJSONArray("Placemark").getJSONObject(0).getJSONObject("AddressDetails").getJSONObject("Country").getJSONObject("AdministrativeArea").getJSONObject("Locality").getString("LocalityName");
-			StringBuffer sb=new StringBuffer(str);
-			sb.deleteCharAt(sb.length()-1);
-			//String picture=new JSONObject(googleJsonStr).getJSONArray("seeRecords").getJSONObject(0).getJSONObject("dbCollection").getString("picture");
-			return sb.toString();
-			
+		inStream.close();
+		return outStream.toByteArray();
+	}
+
+	/**
+	 * 把url请求的资源拷贝到指定文件中去
+	 * @param url
+	 * @param toFile
+	 * @throws Exception
+	 */
+	public static void copyResourceFromUrl(URL url, File toFile) throws Exception {
+		HttpURLConnection conn2 = (HttpURLConnection)url.openConnection();
+		if (conn2.getResponseCode() == 200) {
+			InputStream is = conn2.getInputStream();
+			String str = new String(OtherUtil.read(is), "UTF-8");
+			PrintWriter pw = new PrintWriter(toFile, "UTF-8");
+			pw.print(str);
+			pw.close();
 		}
+	}
+
 }
