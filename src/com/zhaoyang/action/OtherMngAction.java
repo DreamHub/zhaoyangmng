95cdfc34cb51a61644099c83aec70b5a00e9bc26
DreamHub <1169286799@qq.com>
2013-10-27 05:35:59 -0700

    其他设置

--- src/com/zhaoyang/action/OtherMngAction.java
+++ src/com/zhaoyang/action/OtherMngAction.java
@@ -17,10 +17,60 @@ import com.zhaoyang.orm.Rule;
 
 public class OtherMngAction extends AbstractActionSupport {
 
+	private RuleDao ruleDao;
+	
+	public RuleDao getRuleDao() {
+		return ruleDao;
+	}
+
+	public void setRuleDao(RuleDao ruleDao) {
+		this.ruleDao = ruleDao;
+	}
+
 	@Override
 	public String execute() throws Exception {
 		// TODO Auto-generated method stub
-		return super.execute();
+		return SUCCESS;
 	}
+	private String headImgPath;
+	private File img;
+	private String imgFileName;
 	
+	public File getImg() {
+		return img;
+	}
+
+	public void setImg(File img) {
+		this.img = img;
+	}
+
+	public String getImgFileName() {
+		return imgFileName;
+	}
+
+	public void setImgFileName(String imgFileName) {
+		this.imgFileName = imgFileName;
+	}
+
+	public String getHeadImgPath() {
+		return headImgPath;
+	}
+
+	public void setHeadImgPath(String headImgPath) {
+		this.headImgPath = headImgPath;
+	}
+
+	public String headImgMng()throws Exception {
+		headImgPath=ruleDao.findRuleByRuleId("HeadBgImg").getRuleDef();
+		return SUCCESS;
+	}
+	public String headImgEdit()throws Exception {
+		if(img==null){
+			setErrMsg("不能为空");
+			return SUCCESS;
+		}
+		ruleDao.update("HeadBgImg", saveImg(img, "image", imgFileName));
+		setSucMsg("修改成功");
+		return SUCCESS;
+	}
 }
